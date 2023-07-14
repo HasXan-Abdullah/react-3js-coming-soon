ed75039b3dba8eae32f4fbb6082c3a0f74238587
HasXan_Abdullah <62098346+hassan372@users.noreply.github.com>
2023-07-14 19:32:24 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TextLoader from './TextLoader'
+import font from './fonts/font.json'
+
+function App() {
+
+  return (
+    <>
+  <TextLoader text={'hello'} fontPath={font}/>
+    </>
+  )
+}
+
+export default App
