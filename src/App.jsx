b9769233f714b1a8c1b1fdf1a049435e9e2645a4
Hassan_Abdullah <abdullahblink500@gmail.com>
2023-07-21 19:36:53 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import  { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
@@ -7,6 +7,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 const App = () => {
   const rendererRef = useRef(null);
   const cameraRef = useRef(null);
+  const textMeshRef = useRef(null);
+  const [isDayMode, setIsDayMode] = useState(false);
 
   useEffect(() => {
     init();
@@ -17,17 +19,28 @@ const App = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (textMeshRef.current) {
+      const textMaterial = new THREE.MeshBasicMaterial({ color: isDayMode ? 0x00000 : 0x80D4FF });
+      textMeshRef.current.material = textMaterial;
+    }
+    updateRendererBackgroundColor();
+  }, [isDayMode]);
+
+  const handleModeToggle = () => {
+    setIsDayMode(!isDayMode);
+  };
+
   const init = () => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 15;
+    camera.position.z = 20;
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     rendererRef.current = renderer;
     document.body.appendChild(renderer.domElement);
 
- 
     // Load the font
     const fontLoader = new FontLoader();
     fontLoader.load(
@@ -35,11 +48,11 @@ const App = () => {
       (droidFont) => {
         const textGeometry = new TextGeometry('Coming Soon', {
           size: 2,
-          
           height: 1,
           font: droidFont,
         });
-        const textMaterial = new THREE.MeshNormalMaterial();
+
+        const textMaterial = new THREE.MeshBasicMaterial({ color: isDayMode ? 0xffffff : 0x80D4FF });
         const textMesh = new THREE.Mesh(textGeometry, textMaterial);
 
         // Center the text on the screen
@@ -47,8 +60,9 @@ const App = () => {
         const textWidth = textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x;
         textMesh.position.x = -textWidth / 2;
         textMesh.position.y = 0;
-       
+
         scene.add(textMesh);
+        textMeshRef.current = textMesh;
       }
     );
 
@@ -67,6 +81,7 @@ const App = () => {
     };
 
     animate();
+    updateRendererBackgroundColor();
   };
 
   const handleResize = () => {
@@ -83,8 +98,27 @@ const App = () => {
       renderer.setSize(width, height);
     }
   };
-
-  return null;
+  const updateRendererBackgroundColor = () => {
+    if (rendererRef.current) {
+      const renderer = rendererRef.current;
+      renderer.setClearColor(isDayMode ? 0xffffff : 0x000000, 1);
+    }
+  };
+  return (
+    <>
+       <button
+        style={{
+          position: 'absolute',
+          top: '20px',
+          left: '20px',
+          zIndex: '1', // Ensures the button is above the 3D scene
+        }}
+        onClick={handleModeToggle}
+      >
+        Toggle Day/Night
+      </button>
+    </>
+  );
 };
 
 export default App;
