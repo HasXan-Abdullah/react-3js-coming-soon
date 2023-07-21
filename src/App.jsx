1b41f7f31a68b8259b261cdc904b158ef1a44b62
Hassan_Abdullah <abdullahblink500@gmail.com>
2023-07-21 19:08:10 +0500

    3d font added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,89 @@
-import {extend} from '@react-three/fiber'
-import {FontLoader} from
-import './App.css'
-function App() {
- 
-  return (
-    <>
- 
-    </>
-  )
-}
+import  { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-export default App
+const App = () => {
+  const rendererRef = useRef(null);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    init();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const init = () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 15;
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    rendererRef.current = renderer;
+    document.body.appendChild(renderer.domElement);
+
+ 
+    // Load the font
+    const fontLoader = new FontLoader();
+    fontLoader.load(
+      'node_modules/three/examples/fonts/droid/droid_sans_mono_regular.typeface.json',
+      (droidFont) => {
+        const textGeometry = new TextGeometry('Coming Soon', {
+          size: 2,
+          height: 1,
+          font: droidFont,
+        });
+        const textMaterial = new THREE.MeshNormalMaterial();
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+        // Center the text on the screen
+        textGeometry.computeBoundingBox();
+        const textWidth = textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x;
+        textMesh.position.x = -textWidth / 2;
+        textMesh.position.y = 0;
+       
+        scene.add(textMesh);
+      }
+    );
+
+    // Add OrbitControls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.update();
+
+    cameraRef.current = camera;
+
+    // Animation loop
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      controls.update();
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  };
+
+  const handleResize = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const aspectRatio = width / height;
+
+    if (rendererRef.current && cameraRef.current) {
+      const renderer = rendererRef.current;
+      const camera = cameraRef.current;
+
+      camera.aspect = aspectRatio;
+      camera.updateProjectionMatrix();
+      renderer.setSize(width, height);
+    }
+  };
+
+  return null;
+};
+
+export default App;
